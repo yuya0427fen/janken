37deea3e7eafb9156a2a3980be7a8f5f1921158d
yuya0427fen <yuya0427fen@gmail.com>
2017-04-27 12:39:19 +0900

    コード追加

--- docs/player.js
+++ docs/player.js
@@ -1,3 +1,18 @@
+
+const GU = 0;
+const CHOKI = 1;
+const PA = 2;
+
+
+let matches = 0;
 function action(oppornent){
-  return 1;
+  matches = matches + 1;
+  const remainder = matches % 2;
+  const isEven = remainder == 0;
+  console.log(`${matches} % 2 = ${remainder} : ${isEven}`);
+  if(isEven){
+  	return GU
 }
+return PA
+}
+
